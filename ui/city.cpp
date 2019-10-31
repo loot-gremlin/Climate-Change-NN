842ea5c10f41f58a95892cd29465907f5ee35447
Randy Wang <42878069+wandyrandy@users.noreply.github.com>
2019-10-31 00:05:38 -0400

    Create city.cpp

---
+++ ui/city.cpp
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <string>
+#include "city.h"
+
+City::City()
+{
+    cityName="";
+    state="";
+}
+City::City(std::string c,std::string s){
+    cityName=c;
+    state=
+}
